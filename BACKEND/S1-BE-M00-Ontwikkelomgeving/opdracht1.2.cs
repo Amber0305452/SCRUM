b22394fbdeba16314e4c97686f037ae507130b36
Amber <44774806+Amber0305452@users.noreply.github.com>
2018-11-22 09:31:12 +0100

    Add files via upload

---
+++ BACKEND/S1-BE-M00-Ontwikkelomgeving/opdracht1.2.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht1._1
+{
+    public class Programm
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("*********************");
+            Console.WriteLine("********AMBER********");
+            Console.WriteLine("*********************");
+        }
+    }
+
+
+
+}
