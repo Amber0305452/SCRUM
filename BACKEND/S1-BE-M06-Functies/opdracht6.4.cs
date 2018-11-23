54e59f5c86e36cbb2703e2c4e1f4e039cc0ed0a3
Amber <44774806+Amber0305452@users.noreply.github.com>
2018-11-23 11:23:41 +0100

    Update opdracht6.4.cs

--- BACKEND/S1-BE-M06-Functies/opdracht6.4.cs
+++ BACKEND/S1-BE-M06-Functies/opdracht6.4.cs
@@ -1,47 +1,56 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace opdracht6._4
+namespace ConsoleApp9
 {
     class Program
     {
         static void Main(string[] args)
-           
         {
-            Console.WriteLine("Enter the first number.");
-            double firstNumber = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the second number.");
-            double secondNumber = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(firstNumber + "+" + secondNumber + "=" + Adding(firstNumber, secondNumber));
-            Console.WriteLine(firstNumber + "-" + secondNumber + "=" + Minus(firstNumber, secondNumber));
-            Console.WriteLine(firstNumber + "/" + secondNumber + "=" + Divide(firstNumber, secondNumber));
-            Console.WriteLine(firstNumber + "*" + secondNumber + "=" + Multipli(firstNumber, secondNumber));
-            Console.WriteLine(firstNumber + "%" + secondNumber + "=" + Modulo(firstNumber, secondNumber));
+            //Getting Input of First number
+            Console.WriteLine("Enter first number");
+            float num1 = float.Parse(Console.ReadLine());
+
+            //Getting Input of second number
+            Console.WriteLine("Enter second number");
+            float num2 = float.Parse(Console.ReadLine());
+
+            //Getting the math operator
+            Console.WriteLine("Enter operator");
+            string op = Console.ReadLine();
+            Operation(num1, num2, op);
             Console.ReadKey();
         }
-        static double Adding(double firstNumber, double secondNumber)
+
+        static void Operation(float num1, float num2, string op)
         {
-            double addition = firstNumber + secondNumber;
-            return addition; 
-        }
-        static double Minus(double firstNumber, double secondNumber)
-        {
-            double minus = firstNumber - secondNumber;
-            return minus;
-        }
-        static double Divide(double firstNumber, double secondNumber)
-        {
-            double divide = firstNumber / secondNumber;
-            return divide;
-        }
-        static double Multipli(double firstNumber, double secondNumber)
-        {
-            double multipli = firstNumber * secondNumber;
-            return multipli;
-        }
-        static double Modulo(double firstNumber, double secondNumber)
-        {
-            double modulo = firstNumber % secondNumber;
-            return modulo;
+            float result = 0;
+            switch (op)
+            {
+
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                case "%":
+                    result = num1 % num2;
+                    break;
+
+
+
+            }
+            Console.WriteLine("Result = " + result);
         }
     }
 }
