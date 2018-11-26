8b5be41875a5c3b66b9a2124ceb0c5b79dc7ea29
Amber <44774806+Amber0305452@users.noreply.github.com>
2018-11-26 10:52:12 +0100

    Update opdracht6.9.cs

--- BACKEND/S1-BE-M06-Functies/opdracht6.9.cs
+++ BACKEND/S1-BE-M06-Functies/opdracht6.9.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Media; 
 
 
 namespace Opdracht_9_Example
@@ -14,12 +15,29 @@ namespace Opdracht_9_Example
         {
             while (true)
             {
-                string font = Figgle.FiggleFonts.Katakana.Render(DateTime.Now.ToString("h : mm : ss tt"));
+                //Convert time to a figglefoint so it displays the time big with *'s               
+                string font = Figgle.FiggleFonts.Katakana.Render(DateTime.Now.ToString("h | mm | ss tt"));
                 font = font.Replace("#", "*");
+                //displays the time in the selected font
                 Console.WriteLine(font);               
                 Thread.Sleep(1000);
                 Console.Clear();
+                //calls the function for the sound
+                Playsound();
             }
-        }       
+        }
+        static void Playsound()
+        {
+            //Selects current time and gets seconds and minutes from it.
+            DateTime Now = DateTime.Now;
+            int seconds = Now.Second;
+            int minutes = Now.Minute;
+            //When both the minutes and seconds equal 0 it plays a cuckoo sound
+            if(seconds==0 && minutes == 0)
+            {                
+                SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\Amber\source\repos\opdracht6.9\Opdracht9Test\cuckoo.wav");
+                simpleSound.Play();
+            }
+        }
     }
 }
