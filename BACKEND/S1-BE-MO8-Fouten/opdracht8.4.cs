2e6f87162160c1ef628cd5629f2e7371ec5d897f
Amber <44774806+Amber0305452@users.noreply.github.com>
2018-11-26 14:06:29 +0100

    Create opdracht8.4.cs

---
+++ BACKEND/S1-BE-MO8-Fouten/opdracht8.4.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an IP-Address.");
+            string ipAddress = Console.ReadLine(); 
+            /*
+             * Method to have a ping that works for sure.
+            using (var ping = new Ping())
+            {
+                var reply = ping.Send(IPAddress.Loopback);
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    Console.WriteLine("Pinging with server: " + reply.Address);
+                    Console.WriteLine("Press any key to continue");
+                    Console.ReadKey(true);
+                }            
+            }*/
+            
+            //Creates a new ping. 
+            Ping ping = new Ping();
+            //Creates a pingreply, sending the inserted ipaddress here.
+            PingReply pingReply = ping.Send(ipAddress);
+            //If the reply is a succes it will state the ip is reachable and else not.
+            if (pingReply.Status == IPStatus.Success)
+            {
+                Console.WriteLine("This IP is reachable.");
+            }
+            else
+            {
+                Console.WriteLine("This IP is not reachable.");
+            }
+            Console.ReadKey();
+        }        
+    }
+}
